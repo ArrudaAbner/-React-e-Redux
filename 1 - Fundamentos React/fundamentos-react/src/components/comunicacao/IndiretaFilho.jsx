1084e37318c1b943f33290ff67899278228ae730
Abner Rodrigues de Arruda <62723779+ArrudaAbner@users.noreply.github.com>
2023-06-26 00:05:29 -0300

    Componente com estado

--- 1 - Fundamentos React/fundamentos-react/src/components/comunicacao/IndiretaFilho.jsx
+++ 1 - Fundamentos React/fundamentos-react/src/components/comunicacao/IndiretaFilho.jsx
@@ -1,10 +1,17 @@
 /* eslint-disable import/no-anonymous-default-export */
 
 export default (props) => {
+  const min = 50;
+  const max = 70;
+  const gerarIdade = () => parseInt(Math.random() * 20) + 50;
+  const gerarNerd = () => Math.random() > 0.5;
+
   return (
     <div>
       <div>Filho</div>
-      <button onClick={(_) => props.quandoClicar("João", 53, true)}>
+      <button
+        onClick={(_) => props.quandoClicar("João", gerarIdade, gerarNerd)}
+      >
         Fornecer Informações
       </button>
     </div>
--- 1 - Fundamentos React/fundamentos-react/src/components/comunicacao/IndiretaPai.jsx
+++ 1 - Fundamentos React/fundamentos-react/src/components/comunicacao/IndiretaPai.jsx
@@ -1,18 +1,16 @@
 /* eslint-disable import/no-anonymous-default-export */
+import React, { useState } from "react";
 import IndiretaFilho from "./IndiretaFilho";
-import DiretaFilho from "./IndiretaFilho";
 
 export default (props) => {
-  let nome = "?";
-  let idade = 0;
-  let nerd = false;
+  const [nome, setNome] = useState("?");
+  const [idade, setIdade] = useState(0);
+  const [nerd, setNerd] = useState(false);
 
-  function fornecerInformacoes(nomeParam, idadeParam, nerdParam) {
-    nome = nomeParam;
-    idade = idadeParam;
-    nerd = nerdParam;
-
-    console.log(nomeParam, idadeParam, nerdParam);
+  function fornecerInformacoes(nome, idade, nerd) {
+    setNome(nome);
+    setIdade(idade);
+    setNerd(nerd);
   }
 
   return (
