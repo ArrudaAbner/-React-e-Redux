1640a4892319dfd177fff7ba0314f990b557507c
Abner Rodrigues de Arruda <62723779+ArrudaAbner@users.noreply.github.com>
2023-06-27 11:58:08 -0300

    Projeto Calculadora

--- 2 - Calculadora/calculadora-react/src/components/Button.jsx
+++ 2 - Calculadora/calculadora-react/src/components/Button.jsx
@@ -3,5 +3,17 @@ import React from "react";
 import "./Button.css";
 
 export default (props) => {
-  return <button className="button">{props.label}</button>;
+  let classes = "button ";
+  classes += props.operation ? "operation" : "";
+  classes += props.double ? "double" : "";
+  classes += props.triple ? "triple" : "";
+
+  return (
+    <button
+      onClick={(e) => props.click && props.click(props.label)}
+      className={classes}
+    >
+      {props.label}
+    </button>
+  );
 };
