12f025ebf5991288d74db4b85af8f9d662bff42c
Abner Rodrigues de Arruda <62723779+ArrudaAbner@users.noreply.github.com>
2023-07-03 12:00:55 -0300

    UseState #01

--- 4 - React Hooks/react-hooks/src/views/examples/UseState.jsx
+++ 4 - React Hooks/react-hooks/src/views/examples/UseState.jsx
@@ -1,15 +1,45 @@
-import React from 'react'
-import PageTitle from '../../components/layout/PageTitle'
+import React, { useState } from "react";
+import PageTitle from "../../components/layout/PageTitle";
+import SectionTitle from "../../components/layout/SectionTitle";
 
 const UseState = (props) => {
-    return (
-        <div className="UseState">
-            <PageTitle
-                title="Hook UseState"
-                subtitle="Estado em componentes funcionais!"
-            />
-        </div>
-    )
-}
+  const [count, setCount] = useState(0);
 
-export default UseState
+  return (
+    <div className="UseState">
+      <PageTitle
+        title="Hook UseState"
+        subtitle="Estado em componentes funcionais!"
+      />
+
+      <SectionTitle title="Exercício #01"></SectionTitle>
+      <div className="center">
+        <span className="text">{count}</span>
+        <div>
+          <button className="btn" onClick={() => setCount((current) => 0)}>
+            0
+          </button>
+
+          <button className="btn" onClick={() => setCount(count - 1)}>
+            -1
+          </button>
+
+          <button className="btn" onClick={() => setCount(count + 1)}>
+            +1
+          </button>
+
+          <button
+            className="btn"
+            onClick={() => setCount((current) => current + 1000)}
+          >
+            1000
+          </button>
+        </div>
+      </div>
+
+      <SectionTitle title="Exercício #02"></SectionTitle>
+    </div>
+  );
+};
+
+export default UseState;
