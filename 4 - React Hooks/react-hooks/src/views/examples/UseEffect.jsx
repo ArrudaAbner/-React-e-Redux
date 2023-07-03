bff49059983ad65b0786d5c518ae2d25c634fd0b
Abner Rodrigues de Arruda <62723779+ArrudaAbner@users.noreply.github.com>
2023-07-03 17:24:57 -0300

    Exercicio useEffect

--- 4 - React Hooks/react-hooks/src/views/examples/UseEffect.jsx
+++ 4 - React Hooks/react-hooks/src/views/examples/UseEffect.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import PageTitle from "../../components/layout/PageTitle";
+import SectionTitle from "../../components/layout/SectionTitle";
 
+/* Exercicio 1  */
 function calcFatorial(num) {
   const n = parseInt(num);
   if (n < 0) return "Somente positivos";
@@ -8,7 +10,18 @@ function calcFatorial(num) {
   return calcFatorial(n - 1) * n;
 }
 
-const UseEffect = (props) => {
+/* Exercicio 2  */
+function calcPar(num) {
+  const n = parseInt(num);
+  if (n % 2 === 0) {
+    return "Par";
+  } else {
+    return "Impar";
+  }
+}
+
+const UseEffect = () => {
+  /* Exercicio 1  */
   const [number, setNumber] = useState(1);
   const [fatorial, setFatorial] = useState(1);
 
@@ -19,6 +32,17 @@ const UseEffect = (props) => {
     [number]
   );
 
+  /* Exercicio 2  */
+  const [num, setNum] = useState(0);
+  const [par, setPar] = useState(1);
+
+  useEffect(
+    function () {
+      setPar(calcPar(num));
+    },
+    [num]
+  );
+
   return (
     <div className="UseEffect">
       <PageTitle
@@ -26,6 +50,7 @@ const UseEffect = (props) => {
         subtitle="Permite executar efeitos colaterais em componentes funcionais!"
       />
 
+      <SectionTitle title="Exercício #01"></SectionTitle>
       <div className="center">
         <div>
           <span className="text">Fatorial: </span>
@@ -47,6 +72,21 @@ const UseEffect = (props) => {
             -1
           </button>
         </div>
+      </div>
+
+      <SectionTitle title="Exercício #02"></SectionTitle>
+
+      <div className="center">
+        <div>
+          <span className="text">Par ou impar: </span>
+          <span className="text red">{par}</span>
+        </div>
+        <input
+          type="number"
+          className="input"
+          value={num}
+          onChange={(e) => setNum(e.target.value)}
+        />
       </div>
     </div>
   );
