3ed6d476e2b6143ebbc390bf21ce42e8883efb40
Abner Rodrigues de Arruda <62723779+ArrudaAbner@users.noreply.github.com>
2023-07-06 20:42:44 -0300

    Componente layout

--- 5 - Projeto Cadastro/fundamentos/src/pages/estiloso.jsx
+++ 5 - Projeto Cadastro/fundamentos/src/pages/estiloso.jsx
@@ -1,11 +1,12 @@
-import styles from"../styles/Estiloso.module.css"
-import Link from "next/link";
+import Layout from "@/components/Layout";
+import styles from "../styles/Estiloso.module.css";
 
 export default function Estiloso() {
-    return(
-        <div className={styles.roxo}>
-            <a href="/">Voltar</a>
-            <h1>Estilo usando CSS Módulos</h1>
-        </div>
-    )
+  return (
+    <Layout titulo={"Exemplo de CSS Modularizado"}>
+      <div className={styles.roxo}>
+        <h1>Estilo usando CSS Módulos</h1>
+      </div>
+    </Layout>
+  );
 }
