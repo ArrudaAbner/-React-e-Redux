6d95a2b530dc8872a42d39e2c9a2ffe435e8b342
Abner Rodrigues de Arruda <62723779+ArrudaAbner@users.noreply.github.com>
2023-07-06 16:19:19 -0300

    personal hook

--- 4 - React Hooks/react-hooks/src/views/examples/UseCustom.jsx
+++ 4 - React Hooks/react-hooks/src/views/examples/UseCustom.jsx
@@ -2,9 +2,25 @@ import React from "react";
 import PageTitle from "../../components/layout/PageTitle";
 import SectionTitle from "../../components/layout/SectionTitle";
 import { useCounter } from "../../hooks/UseCounter";
+import { useFetch } from "../../hooks/UseFetch";
 
 const UseRef = (props) => {
   const [count, inc, dec] = useCounter(100);
+  const url = "http://files.cod3r.com.br/curso-react/estados.json";
+  const response = useFetch(url);
+
+  function showStates(states) {
+    return states.map((state) => (
+      <li key={state.nome}>
+        {state.nome} - {state.sigla}
+      </li>
+    ));
+  }
+
+  //http://files.cod3r.com.br/curso-react/estados.json
+  /* fetch("http://files.cod3r.com.br/curso-react/estados.json")
+    .then((resp) => resp.json())
+    .then((json) => console.log(json)); */
 
   return (
     <div className="UseCustom">
@@ -24,6 +40,11 @@ const UseRef = (props) => {
             - 1
           </button>
         </div>
+      </div>
+
+      <SectionTitle title="Exercício #01"></SectionTitle>
+      <div className="center">
+        <ul> {response.data ? showStates(response.data) : false}</ul>
       </div>
     </div>
   );
