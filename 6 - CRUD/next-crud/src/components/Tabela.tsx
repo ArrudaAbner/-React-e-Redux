8456dd882d2d2d9086c5cd97b9b0f5c06fc959c5
Abner Rodrigues de Arruda <62723779+ArrudaAbner@users.noreply.github.com>
2023-07-07 22:55:42 -0300

    V1.0.4
    tabela #02

--- 6 - CRUD/next-crud/src/components/Tabela.tsx
+++ 6 - CRUD/next-crud/src/components/Tabela.tsx
@@ -8,9 +8,9 @@ export default function Tabela(props: TabelaProps) {
   function renderizarCabecalho() {
     return (
       <tr>
-        <th>Código</th>
-        <th>Nome</th>
-        <th>Idade</th>
+        <th className="text-left p-4">Código</th>
+        <th className="text-left p-4">Nome</th>
+        <th className="text-left p-4">Idade</th>
         {/* <th>Ações</th> */}
       </tr>
     );
@@ -19,21 +19,27 @@ export default function Tabela(props: TabelaProps) {
   function renderizarDados() {
     return props.clientes?.map((cliente, i) => {
       return (
-        <tr key={cliente.getId}>
-          <td>{cliente.getId}</td>
-          <td>{cliente.getNome}</td>
-          <td>{cliente.getIdade}</td>
+        <tr key={cliente.getId}
+        className={`${i % 2 === 0 ? "bg-purple-200" : "bg-purple-100"}`}>
+          <td className="text-left p-4">{cliente.getId}</td>
+          <td className="text-left p-4">{cliente.getNome}</td>
+          <td className="text-left p-4">{cliente.getIdade}</td>
         </tr>
       );
     });
   }
 
   return (
-    <table>
-      <thead>{renderizarCabecalho()}</thead>
-      <tbody>
-        {renderizarDados()}
-      </tbody>
+    <table className="w-full rounded-xl overflow-hidden">
+      <thead
+        className={`
+      text-gray-100
+      bg-gradient-to-r from-purple-500 to-purple-800
+      `}
+      >
+        {renderizarCabecalho()}
+      </thead>
+      <tbody>{renderizarDados()}</tbody>
     </table>
   );
 }
