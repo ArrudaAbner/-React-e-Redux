03f210be58bd560b34a38392c5d34e1093ec800d
Abner Rodrigues de Arruda <62723779+ArrudaAbner@users.noreply.github.com>
2023-07-07 21:19:08 -0300

    Finalizando

--- 5 - Projeto Cadastro/fundamentos/src/pages/index.jsx
+++ 5 - Projeto Cadastro/fundamentos/src/pages/index.jsx
@@ -18,6 +18,7 @@ export default function Inicio() {
       <Navegador texto="Navegação #02" destino="/cliente/sp-2/123" cor="orange"></Navegador>
       <Navegador texto="Componente com Estado" destino="/estado" cor="pink"></Navegador>
       <Navegador texto="Integração 1" destino="/integracao_1" cor="#03052e"></Navegador>
+      <Navegador texto="Conteúdo Estático" destino="/estatico" cor="#fa054a"></Navegador>
     </div>
   );
 }
