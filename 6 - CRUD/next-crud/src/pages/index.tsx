9f0e31cfa33cf1b11746340fe75728d335182853
Abner Rodrigues de Arruda <62723779+ArrudaAbner@users.noreply.github.com>
2023-07-07 22:24:03 -0300

    1.0.1
    Componente Layout e Titulo

--- 6 - CRUD/next-crud/src/pages/index.tsx
+++ 6 - CRUD/next-crud/src/pages/index.tsx
@@ -1,12 +1,21 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import Layout from "@/components/Layout";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <div>
-      
+    <div
+      className={`
+      flex justify-center items-center h-screen
+      bg-gradient-to-r from-blue-500 to-purple-500
+      text-white
+    `}
+    >
+      <Layout titulo="Cadastro Simples">
+        <span>Conteúdo</span>
+      </Layout>
     </div>
   );
 }
