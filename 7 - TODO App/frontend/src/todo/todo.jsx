d24c39ea813e6a22959157894bb1dd2f86fae200
Abner Rodrigues de Arruda <62723779+ArrudaAbner@users.noreply.github.com>
2023-07-10 17:10:51 -0300

    Todo App - Frontend
    Evento Adicionar

--- 7 - TODO App/frontend/src/todo/todo.jsx
+++ 7 - TODO App/frontend/src/todo/todo.jsx
@@ -4,12 +4,21 @@ import TodoForm from "./todoForm";
 import TodoList from "./todoList";
 
 export default class Todo extends Component {
+  constructor(props) {
+    super(props);
+    this.handleAdd = this.handleAdd.bind(this);
+  }
+
+  handleAdd() {
+    console.log(this);
+  }
+
   render() {
     return (
       <div>
-        <PageHeader name="Tarefas" small="Cadastro"></PageHeader>
-        <TodoForm></TodoForm>
-        <TodoList></TodoList>
+        <PageHeader name="Tarefas" small="Cadastro" />
+        <TodoForm handleAdd={this.handleAdd} />
+        <TodoList />
       </div>
     );
   }
--- 7 - TODO App/frontend/src/todo/todoForm.jsx
+++ 7 - TODO App/frontend/src/todo/todoForm.jsx
@@ -13,7 +13,11 @@ export default (props) => {
         ></input>
       </Grid>
       <Grid cols="12 3 2">
-        <IconButton style="primary" icon="plus"></IconButton>
+        <IconButton
+          style="primary"
+          icon="plus"
+          onClick={props.handleAdd}
+        ></IconButton>
       </Grid>
     </div>
   );
