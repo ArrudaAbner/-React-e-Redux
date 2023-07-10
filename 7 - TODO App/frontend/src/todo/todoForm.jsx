50d06dfe6514a3ac95b74b1b9e9b625b0fce41ca
Abner Rodrigues de Arruda <62723779+ArrudaAbner@users.noreply.github.com>
2023-07-10 15:46:26 -0300

    Todo-App Frontend
    Estrutura do formulário

--- 7 - TODO App/frontend/src/todo/todoForm.jsx
+++ 7 - TODO App/frontend/src/todo/todoForm.jsx
@@ -2,8 +2,20 @@ import React from "react";
 
 export default (props) => {
   return (
-    <div>
-      <h1>Form</h1>
+    <div role="form" className="todoForm">
+      <div className="col-xs-12 col-sm-9 col-md-10">
+        <input
+          id="description"
+          className="form-control"
+          placeholder="Adicione uma tarefa"
+        />
+      </div>
+
+      <div className="col-xs-12 col-sm-3 col-md-2">
+        <button className="btn btn-primary">
+          <i className="fa fa-plus"></i>
+        </button>
+      </div>
     </div>
   );
 };
--- 7 - TODO App/frontend/src/todo/todoList.jsx
+++ 7 - TODO App/frontend/src/todo/todoList.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default (props) => {
   return (
     <div>
-      <h1>Lista</h1>
+      <h1>List</h1>
     </div>
   );
 };
