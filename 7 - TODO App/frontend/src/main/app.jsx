bb7f0a6d79771fbc72b37909fcb8cb7305282d90
Abner Rodrigues de Arruda <62723779+ArrudaAbner@users.noreply.github.com>
2023-07-10 12:56:38 -0300

    Todo-APP Frontend
    Componente APP

---
+++ 7 - TODO App/frontend/src/main/app.jsx
@@ -0,0 +1,12 @@
+import "modules/bootstrap/dist/css/bootstrap.min.css";
+import "modules/font-awesome/css/font-awesome.min.css";
+
+import React from "react";
+
+export default (props) => {
+  return (
+    <div className="container">
+      <h1>teste</h1>
+    </div>
+  );
+};
