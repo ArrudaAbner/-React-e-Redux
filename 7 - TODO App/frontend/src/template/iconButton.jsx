7f9dd38eaeb51b16c274a6768c1a7635630921d4
Abner Rodrigues de Arruda <62723779+ArrudaAbner@users.noreply.github.com>
2023-07-10 16:41:33 -0300

    Todo App - Frontend
    Componentes Grid, iconButton e IF

---
+++ 7 - TODO App/frontend/src/template/iconButton.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import If from "./If";
+
+export default (props) => {
+  return (
+    <If test={!props.hide}>
+      <button className={"btn btn-" + props.style} onClick={props.onClick}>
+        <i className={"fa fa-" + props.icon}></i>
+      </button>
+    </If>
+  );
+};
--- 7 - TODO App/frontend/src/todo/todoForm.jsx
+++ 7 - TODO App/frontend/src/todo/todoForm.jsx
@@ -1,21 +1,20 @@
 import React from "react";
+import Grid from "../template/grid";
+import IconButton from "../template/iconButton";
 
 export default (props) => {
   return (
     <div role="form" className="todoForm">
-      <div className="col-xs-12 col-sm-9 col-md-10">
+      <Grid cols="12 9 10">
         <input
           id="description"
           className="form-control"
           placeholder="Adicione uma tarefa"
-        />
-      </div>
-
-      <div className="col-xs-12 col-sm-3 col-md-2">
-        <button className="btn btn-primary">
-          <i className="fa fa-plus"></i>
-        </button>
-      </div>
+        ></input>
+      </Grid>
+      <Grid cols="12 3 2">
+        <IconButton style="primary" icon="plus"></IconButton>
+      </Grid>
     </div>
   );
 };
