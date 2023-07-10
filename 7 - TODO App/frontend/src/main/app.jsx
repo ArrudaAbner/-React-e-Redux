1b7d0811feadee964932a2236d4fd2d69eb8625e
Abner Rodrigues de Arruda <62723779+ArrudaAbner@users.noreply.github.com>
2023-07-10 15:19:50 -0300

    Todo-App Front-end
    Configurando as Rotas com react-router

--- 7 - TODO App/frontend/src/main/app.jsx
+++ 7 - TODO App/frontend/src/main/app.jsx
@@ -2,16 +2,15 @@ import "modules/bootstrap/dist/css/bootstrap.min.css";
 import "modules/font-awesome/css/font-awesome.min.css";
 
 import React from "react";
-import Todo from "../todo/todo";
-import About from "../about/about";
+
 import Menu from "../template/menu";
+import Routes from "./routes";
 
 export default (props) => {
   return (
     <div className="container">
       <Menu></Menu>
-      <Todo></Todo>
-      <About></About>
+      <Routes></Routes>
     </div>
   );
 };
