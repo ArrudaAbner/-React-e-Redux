6a06872f26505dcc69c1160b03aef13bf950926b
Abner Rodrigues de Arruda <62723779+ArrudaAbner@users.noreply.github.com>
2023-07-12 17:30:02 -0300

    Redux
    Criando projeto base

---
+++ 8 - Redux/redux/src/components/Card.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable import/no-anonymous-default-export */
+import "./Card.css";
+
+import React from "react";
+
+export default () => {
+  return (
+    <div>
+      Card
+    </div>
+  );
+};
