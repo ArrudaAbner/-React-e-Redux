b92d728133de843bd84d6eb0600607aa69091479
Abner Rodrigues de Arruda <62723779+ArrudaAbner@users.noreply.github.com>
2023-07-12 18:12:53 -0300

    Redux
    Componente Card

--- 8 - Redux/redux/src/components/Card.jsx
+++ 8 - Redux/redux/src/components/Card.jsx
@@ -3,10 +3,21 @@ import "./Card.css";
 
 import React from "react";
 
-export default () => {
+function getColor(props) {
+  if (props.red) return "Red";
+  if (props.green) return "Green";
+  if (props.blue) return "Blue";
+  if (props.purple) return "Purple";
+  return "";
+}
+
+export default (props) => {
   return (
-    <div>
-      Card
+    <div className={`Card ${getColor(props)}`}>
+      <div className="Header">
+        <span className="Title">{props.title}</span>
+      </div>
+      <div className="Content">{props.children}</div>
     </div>
   );
 };
