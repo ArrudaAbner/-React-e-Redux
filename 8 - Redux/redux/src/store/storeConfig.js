ada15bf7774bb41e8de1339e28d3e9edca03ff51
Abner Rodrigues de Arruda <62723779+ArrudaAbner@users.noreply.github.com>
2023-07-13 23:21:16 -0300

    Redux
    Configurando o Redux

---
+++ 8 - Redux/redux/src/store/storeConfig.js
@@ -0,0 +1,20 @@
+import { createStore, combineReducers } from "redux";
+
+const reducers = combineReducers({
+  numeros: function (state, action) {
+    console.log(state, " ", action);
+    return {
+      min: 1,
+      max: 10,
+    };
+  },
+  nomes: function (state, action) {
+    return ["Ana", "Bia", "Carlos"];
+  },
+});
+
+function configStore() {
+  return createStore(reducers);
+}
+
+export default configStore;
