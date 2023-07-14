b5e48301c841b6f5778eee63387ee1521620c29b
Abner Rodrigues de Arruda <62723779+ArrudaAbner@users.noreply.github.com>
2023-07-14 00:43:12 -0300

    Redux
    Refatorando Reducer

--- 8 - Redux/redux/src/store/storeConfig.js
+++ 8 - Redux/redux/src/store/storeConfig.js
@@ -1,28 +1,8 @@
 import { createStore, combineReducers } from "redux";
+import numerosReducer from "./reducers/numerosReducer";
 
 const reducers = combineReducers({
-  numeros: function (state, action) {
-    switch (action.type) {
-      case "NUM_MIN_ALTERADO":
-        return {
-          ...state,
-          min: action.payload,
-        };
-      case "NUM_MAX_ALTERADO":
-        return {
-          ...state,
-          max: action.payload,
-        };
-      default:
-        return {
-          min: 7,
-          max: 31,
-        };
-    }
-  },
-  nomes: function (state, action) {
-    return ["Ana", "Bia", "Carlos"];
-  },
+  numeros: numerosReducer,
 });
 
 function configStore() {
