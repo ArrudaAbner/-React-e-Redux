bd298cfce4155908b58cef3913e9f8cfa05f482c
Abner Rodrigues de Arruda <62723779+ArrudaAbner@users.noreply.github.com>
2023-07-13 23:40:38 -0300

    Redux
    Conectando componentes com Redux

--- 8 - Redux/redux/src/components/Media.jsx
+++ 8 - Redux/redux/src/components/Media.jsx
@@ -1,17 +1,27 @@
 /* eslint-disable import/no-anonymous-default-export */
-
 import Card from "./Card";
 import React from "react";
+import { connect } from "react-redux";
 
-export default (props) => {
+const Media = (props) => {
+  const { min, max } = props;
   return (
     <Card title="Média dos Números" green>
       <div>
         <span>
           <span>Resultado:</span>
-          <strong>{10}</strong>
+          <strong>{(max + min) / 2}</strong>
         </span>
       </div>
     </Card>
   );
 };
+
+function mapStateToProps(state) {
+  return {
+    min: state.numeros.min,
+    max: state.numeros.max,
+  };
+}
+
+export default connect(mapStateToProps)(Media);
--- 8 - Redux/redux/src/store/storeConfig.js
+++ 8 - Redux/redux/src/store/storeConfig.js
@@ -4,8 +4,8 @@ const reducers = combineReducers({
   numeros: function (state, action) {
     console.log(state, " ", action);
     return {
-      min: 1,
-      max: 10,
+      min: 7,
+      max: 31,
     };
   },
   nomes: function (state, action) {
